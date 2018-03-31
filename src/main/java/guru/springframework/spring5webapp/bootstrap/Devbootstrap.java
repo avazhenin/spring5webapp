43946b14c9c5b4a318a743e00aa49d9eae265752
avazhenin <vazhenina88@gmail.com>
2018-04-01 01:46:16 +0600

    Added repositories, added constructor and etc...

--- src/main/java/guru/springframework/spring5webapp/bootstrap/Devbootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/Devbootstrap.java
@@ -2,10 +2,22 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Devbootstrap implements ApplicationListener<ContextRefreshedEvent>{
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public Devbootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
@@ -19,10 +31,16 @@ public class Devbootstrap implements ApplicationListener<ContextRefreshedEvent>{
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
         //Rod
         Author rod = new Author("Rod","Jonson");
         Book noEJB = new Book("Java2EE Development without EJB","56789","Worx");
         eric.getBooks().add(noEJB);
         ddd.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
     }
 }
