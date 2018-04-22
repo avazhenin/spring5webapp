cb8f59d9d1990122a0c2321041d3913dc19cb81d
avazhenin <vazhenina88@gmail.com>
2018-04-22 23:28:07 +0600

    added authors controller stuff

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -17,7 +17,6 @@ public class BookController {
     @RequestMapping("/books")
     public String getBooks(Model model){
         model.addAttribute("books", bookRepository.findAll());
-
         return "books";
     }
 
